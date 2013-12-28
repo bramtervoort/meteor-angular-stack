dbb0939737ec75c4c192dbcb5509e96f188be693
Bram Tervoort <b.tervoort@gmail.com>
2013-12-28 01:51:46 +0100

    example angular and meteor app

--- todo.js
+++ todo.js
@@ -1,3 +1,7 @@
+// both
+Todo = new Meteor.Collection('todos');
+
+// server
 if (Meteor.isServer) {
   WebApp.addHtmlAttributeHook(function (request) {
     return 'ng-app="todoapp"';
@@ -6,4 +10,33 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+// client
+if (Meteor.isClient) {
+  var app = angular.module('todoapp', []);
+  app.controller('TodoListCtrl', ['$scope', 
+                         function ($scope) {
+						 
+    Deps.autorun(function (computation) {
+	  $scope.todos = Todo.find().fetch();
+	  
+	  if (!computation.firstRun) {
+	    $scope.$apply();
+	  }
+	});
+	
+	$scope.remove = function (id) {
+	  Todo.remove({_id: id});
+	};
+	
+	$scope.description = "";
+	$scope.add = function () {
+	  Todo.insert({
+	    description: $scope.description, 
+		done: false
+      });
+	  $scope.description = "";
+	};
+  }]);
 }
