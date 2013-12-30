0358dc93851e87b3fe2b09ce66b247dee912b654
Bram Tervoort <b.tervoort@gmail.com>
2013-12-30 09:07:52 +0100

    ahtm is going to be parsed thou my own template engin to enable markup

--- package.js
+++ package.js
@@ -15,6 +15,7 @@ Package.on_test(function (api) {
 
   api.use(['angular-stack', 'tinytest', 'test-helpers'], ['client', 'server']);
   api.add_files('tests/angular_test.js', 'client');
+  
   api.add_files([
     'tests/server_test.js'
   ], 'server');
@@ -24,4 +25,9 @@ Package.on_test(function (api) {
     'plugin/scanner_tests.js'
   ], 'server');
   
+  api.add_files([
+    'tests/test_double.js',
+	'plugin/compile-templates.js',
+	'plugin/test-compile-templates.js'
+  ], 'server');
 });
