63479de731c5fe2b53f21bc0b9f0cc196a454be0
Bram Tervoort <b.tervoort@gmail.com>
2014-01-03 20:02:55 +0000

    scope.autorun adds the results of reactive functions to the scope and manages it properly.

--- client.js
+++ client.js
@@ -1,21 +1,45 @@
-var meteor = angular.module('meteor', []);
+"use strict";
+angular.module('meteor', [])
 
-meteor.factory('meteor', [
+.run(['$rootScope', 'deps', function ($rootScope, deps) {
+	// function to autorun some reactive functions
+	$rootScope.autorun = function (field, fn) {
+		var $scope = this,
+		    computation = deps.autorun((function ($scope, field, fn) {
+				return function (comp) {
+					$scope[field] = fn();
+					
+					if(!comp.firstRun)
+						$scope.$apply();
+				}
+			})($scope, field, fn));
+			
+		// if I don't save the computation it will 
+		// get lost if I run this function again. 	  
+		$scope.$on('$destroy', (function (comp) {
+			return function () {
+				comp.stop();
+			}
+		})(computation));
+	};
+}])
+
+.factory('meteor', [
   function() { return Meteor; }
-]);
+])
 
-meteor.factory('deps', [
+.factory('deps', [
   function() { return Deps; }
-]);
+])
 
-meteor.factory('session', [
+.factory('session', [
   function() { return Session; }
-]);
+])
 
-meteor.factory('match', [
+.factory('match', [
   function() { return Match; }
-]);
+])
 
-meteor.factory('ejson', [
+.factory('ejson', [
   function() { return EJSON; }
 ]);
--- tests/client_test.js
+++ tests/client_test.js
@@ -22,3 +22,53 @@ Tinytest.add('AngularStack - Angular Module - meteor.ejson service should return
   test.equal(EJSON, angular.injector(['meteor', 'ng']).get('ejson'));
 });
 
+Tinytest.add('AngularStack - Angular Module - $scope.autorun reruns and stops properly', function (test) {
+	var injector = angular.injector(['meteor', 'ng']),
+		rootScope = injector.get('$rootScope'),
+		deps = injector.get('deps'),
+		session = injector.get('session'),
+		
+		execA = 0,
+		execB = 0,
+		
+		scopeA = rootScope.$new(),
+		scopeB = rootScope.$new();
+	session.set('sesA', 'somevalue');
+	scopeA.autorun('sesA', function () {
+		++execA;
+		return session.get('sesA');
+	});
+	scopeB.autorun('sesA', function () {
+		++execB;
+		return session.get('sesA');
+	});
+	
+	// first check if the value's get set properly
+	test.equal(1, execA, 'should have executed the value');
+	test.equal(1, execB, 'should have executed the value');
+	test.equal('somevalue', scopeA.sesA, 'should have set the value');
+	test.equal('somevalue', scopeB.sesA, 'should have set the value');
+	
+	session.set('sesA', 'someothervalue');
+	deps.flush(); // force deps to update
+	test.equal(2, execA, 'should have executed the value');
+	test.equal(2, execB, 'should have executed the value');
+	test.equal('someothervalue', scopeA.sesA, 'should have set the value');
+	test.equal('someothervalue', scopeB.sesA, 'should have set the value');
+	
+	scopeA.$destroy();
+	session.set('sesA', 'yet some other value');
+	deps.flush(); // force deps to update
+	test.equal(2, execA, 'should have executed the value');
+	test.equal(3, execB, 'should have executed the value');
+	test.equal('someothervalue', scopeA.sesA, 'should have not set the value');
+	test.equal('yet some other value', scopeB.sesA, 'should have set the value');
+	
+	scopeB.$destroy();
+	session.set('sesA', 'yet some other value extra');
+	deps.flush(); // force deps to update
+	test.equal(2, execA, 'should have executed the value');
+	test.equal(3, execB, 'should have executed the value');
+	test.equal('someothervalue', scopeA.sesA, 'should have not set the value');
+	test.equal('yet some other value', scopeB.sesA, 'should have set the value');
+});
